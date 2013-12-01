0a053f8de4e06c5a9442447b009a9310082079c8
Wenjie <wenjie@umail.ucsb.edu>
2013-11-30 17:33:57 -0800

    check point

--- src/com/microblog/client/FrontClient.java
+++ src/com/microblog/client/FrontClient.java
@@ -13,9 +13,9 @@ public class FrontClient extends Client {
 	public static void main ( String args[])	{
 		
 		try {
-			FrontClient client = new FrontClient ("127.0.0.1",8200);
-			//client.send("POST:test");
-			client.send("accepted:1:1:0:1:test");
+			FrontClient client = new FrontClient ("127.0.0.1",8000);
+			client.send("POST:test");
+			//client.send("accepted:1:1:0:1:test");
 			//System.out.println ( "response: "+ client.receive());
 			
 			
