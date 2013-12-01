d06157d22f99bfd37f03d3a50161e26e0f4618c5
Wenjie <wenjie@umail.ucsb.edu>
2013-11-30 17:21:12 -0800

    add timestamp to post

--- src/com/microblog/server/Post.java
+++ src/com/microblog/server/Post.java
@@ -7,11 +7,13 @@ public class Post {
 	public String message;
 	public Socket socket;
 	public int position;
+	public long timeStamp;
 	
 	public Post (String message, int position, Socket socket)	{
-		this.message  = message;
-		this.position = position;
-		this.socket   = socket;
+		this.message  	= message;
+		this.position 	= position;
+		this.socket   	= socket;
+		this.timeStamp	= 0; 
 	}
 	
 	public String toString ()	{
