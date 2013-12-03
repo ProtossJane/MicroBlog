6662ab59d81433eb54fbc0b535b95a20c8d03368
madi8229 <madi8229@gmail.com>
2013-12-02 20:27:29 -0800

    Run Client pathyon

---
+++ run_Client.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+
+id = sys.argv[1]
+message = sys.argv[2]
+times = int(sys.argv[3])
+
+for i in range(times):
+	subprocess.call(['java', '-jar','Client.jar', id, message]);
